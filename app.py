1b3af395853da095d740f4e60167010b28b8602c
Hafiz Hassan Abdullah <151431823+rajahassan38201@users.noreply.github.com>
2025-12-14 18:49:56 +0500

    Refactor app.py for improved MCQ generation
    Refactor app.py to improve structure and functionality.

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 import json
 from io import BytesIO
 from dotenv import load_dotenv
- 
+
 load_dotenv()  
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.prompts import ChatPromptTemplate
@@ -340,5 +340,6 @@ elif st.session_state.current_state == "upload":
 st.markdown("---")
 
 st.markdown("Developed by Hafiz Hassan Abdullah | Email: rajahassan38201@gmail.com | Phone: +92 302 3536363")
+
 
 
